b863f245894c8f743eec2d5ea66d253335ccbd6c
YurchenkovGA <yurchencow@yandex.ru>
2020-05-26 17:52:34 +0300

    code: Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 #include "histogram.h"
 #include "svg.h"
 using namespace std;
@@ -38,29 +40,39 @@ Input read_input(istream& in, bool prompt) {
     }
     return data;
 }
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    const size_t data_size = item_size * item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return data_size;
+}
 
-int main(int argc, char* argv[]) {
-
-
-    if (argc > 1)
-    {
+Input download(const string& address) {
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
         CURL *curl = curl_easy_init();
         if(curl) {
             CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-
+            curl_easy_setopt(curl, CURLOPT_URL,address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
             res = curl_easy_perform(curl);
             if (res != CURLE_OK ){
                  cerr<<curl_easy_strerror(res);
                  exit(1);
             }
             curl_easy_cleanup(curl);
-        }
-        return 0;
     }
+        return read_input(buffer, false);
+}
+int main(int argc, char* argv[]) {
+    Input input;
+    if (argc > 1)
+        input = download(argv[1]);
+     else
+        input = read_input(cin, true);
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin, true);
 
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
